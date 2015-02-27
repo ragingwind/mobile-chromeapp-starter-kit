545bac965ba4e2328f4f260aa16fddcb8f338b5c
ragingwind <ragingwind@gmail.com>
2015-02-27 17:51:28 +0900

    Remove platform

--- platform/hooks/before_prepare/cca-check.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-var cmdline = process.env['CORDOVA_CMDLINE'];
-if (!/cca/.test(cmdline)) {
-  var msg = 'ERROR: This is a CCA based project! Using `cordova` rather than `cca` will have unexpected results.' ;
-  console.error(msg);
-  process.exit(1);
-}
