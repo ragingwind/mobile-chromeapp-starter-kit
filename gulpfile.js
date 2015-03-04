71f103a9e94d5efa828b4747059d69715e04be31
ragingwind <ragingwind@gmail.com>
2015-03-05 00:58:17 +0900

    Add watch, build, config

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var gutil = require('gulp-util');
-var fs = require('fs');
 var del = require('del');
-var path = require('path');
 var runSequence = require('run-sequence');
 var next = require('next-promise');
 var ccad = require('cca-delegate');
     ccad.options({verbose: true});
 var polymports = require('gulp-polymports');
+// var bowercfg = require('bower-config').read();
 
-var opts = require('minimist')(process.argv.slice(2));
-var bowercfg = require('bower-config').read();
-var cfg = require('./appcfg');
-var platforms = ['chrome', 'android'];
+var config = {
+  target: 'chrome',
+  watch: false,
+  optimize: false,
+  app: 'build'
+};
 
-var styleTask = function (stylesPath, srcs) {
+var styleTask = function(stylesPath, srcs) {
   return gulp.src(srcs.map(function(src) {
       return 'app/' + stylesPath + src;
     }))
@@ -31,7 +32,17 @@ var styleTask = function (stylesPath, srcs) {
     .pipe(gulp.dest('build/' + stylesPath));
 };
 
-gulp.task('jshint', function () {
+gulp.task('configure', function() {
+  var opts = require('minimist')(process.argv.slice(2));
+  config.watch = !!opts.watch;
+  config.optimize = (!!opts.optimize || !!opts.o);
+  config.app = opts.app;
+  if (opts.target) {
+    config.target = opts.target;
+  }
+});
+
+gulp.task('jshint', function() {
   return gulp.src([
     'app/scripts/**/*.js',
     'app/elements/**/*.js',
@@ -91,7 +102,7 @@ gulp.task('copy:elements', function() {
 
 gulp.task('vulcanize:common', function() {
   var dest = './build/elements';
-  polymports.src(require('./appcfg.json').bundles)
+  polymports.src(require('./vulcanize').bundles)
     .pipe($.vulcanize({
       dest: dest,
       csp: true,
@@ -113,25 +124,12 @@ gulp.task('vulcanize:elements', function() {
     .pipe(gulp.dest(dest));
 });
 
-gulp.task('html', ['styles'], function () {
-  var assets = $.useref.assets({searchPath: ['app']});
-  return gulp.src('build/**/*.html')
-    .pipe(assets)
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.csso()))
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
-    .pipe(gulp.dest('build'));
-});
-
 gulp.task('html', function () {
-  var assets = $.useref.assets({searchPath: ['.tmp', 'app', 'build']});
+  var assets = $.useref.assets({searchPath: ['app']});
   return gulp.src([
       'app/**/*.html',
       '!app/{elements,bower_components}/**/*.html'
     ])
-    // .pipe($.if('*.html', $.replace('elements/elements.html', 'elements/elements.vulcanized.html')))
     .pipe(assets)
     .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
     .pipe($.if('*.css', $.cssmin()))
@@ -145,83 +143,72 @@ gulp.task('html', function () {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('elements:release', function(cb) {
-  runSequence(['styles:elements', 'copy:elements'], 'vulcanize:elements', cb);
-});
+gulp.task('vulcanize', function(cb) {
+  var tasks = [
+    ['vulcanize:common', 'styles:elements', 'copy:elements'],
+    cb
+  ];
 
-gulp.task('elements:debug', function(cb) {
-  runSequence(['styles:elements', 'copy:elements'], cb);
-});
-
-var watch = function(vulcan) {
-  var elementsTask = ['jshint', 'copy:elements'];
-
-  if (vulcan) {
-    elementsTask.splice(1, 0, 'vulcanize:elements');
+  if (config.optimize) {
+    tasks.splice(1, 0, 'vulcanize:elements');
   }
 
-  gulp.watch(['app/**/*.html', '!app/elements/**/*.html'], ['copy']);
-  gulp.watch(['app/scripts/**/*.js'], ['jshint', 'copy']);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles']);
-  gulp.watch(['app/images/**/*'], ['images']);
-  gulp.watch(['app/elements/**/*.{scss,css}'], ['styles:elements']);
-  gulp.watch(['app/elements/**/*.{js,html}'], [vulcan ? 'elements:release' : 'elements:debug']);
-  gulp.watch(['appcfg.json'], ['vulcanize:common']);
-};
-
-gulp.task('watch', function(cb) {
-  watch();
+  runSequence.apply(runSequence, tasks);
 });
 
-gulp.task('clean', del.bind(null, ['.tmp', 'build']));
-
-gulp.task('build:release', ['jshint'], function() {
-  runSequence(['copy', 'copy:bower', 'styles', 'images', 'html',],
-    'vulcanize:common', 'elements:release');
-  if (opts.watch) {
-    watch(true);
-  }
+gulp.task('clean', function(cb) {
+  del.bind(null, ['.tmp', 'build']));
+  cache.clearAll(cb);
 });
 
-gulp.task('build:debug', ['jshint'], function() {
-  // will be replaced to gulp.start or other
-  runSequence(['copy', 'copy:bower', 'styles', 'images'],
-    'vulcanize:common', 'elements:debug');
-  if (opts.watch) {
-    watch(true);
-  }
-});
+gulp.task('build', ['configure', 'jshint'], function(cb) {
+  var tasks = [
+    ['copy', 'copy:bower', 'styles', 'images'],
+    'vulcanize'
+  ];
 
-gulp.task('build', ['clean'], function() {
-  gulp.start('build:debug');
-});
-
-gulp.task('run', function(cb) {
-  var platformsForRunning = [];
-
-  platforms.forEach(function(p) {
-    if (opts[p]) {
-      platformsForRunning.push(p);
-    }
-  });
-
-  if (platformsForRunning.length === 0) {
-    platformsForRunning.push('chrome');
+  if (config.optimize) {
+    tasks.push('html');
   }
 
-  next(platformsForRunning, function(p) {
-    return ccad.run({
-      platform: p,
+  tasks.push(cb);
+
+  if (config.watch) {
+    gulp.watch(['app/**/*.html', '!app/elements/**/*.html'], ['copy']);
+    gulp.watch(['app/scripts/**/*.js'], ['jshint', 'copy']);
+    gulp.watch(['app/styles/**/*.{scss,css}'], ['styles']);
+    gulp.watch(['app/images/**/*'], ['images']);
+    gulp.watch(['app/elements/**/*.{scss,css}'], ['styles:elements']);
+    gulp.watch(['app/elements/**/*.{js,html}'], ['vulcanize']);
+    gulp.watch(['vulcanize.json'], ['vulcanize:common']);
+  }
+
+  runSequence.apply(runSequence, tasks);
+});
+
+gulp.task('run', ['configure'], function() {
+  var run = function() {
+    ccad.run({
+      platform: config.target,
       cwd: './platform'
+    }).then(null, function(err) {
+      err & console.log(err.toString());
     });
-  }).then(function() {
-    // runSequence('watch', cb);
-  }, function(err) {
-    err & console.error(err.toString());
-    cb();
-  });
+  }
+
+  if (config.watch) {
+    gulp.watch(['app/**/*.html', '!app/elements/**/*.html'], ['copy', run]);
+    gulp.watch(['app/scripts/**/*.js'], ['jshint', 'copy', run]);
+    gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', run]);
+    gulp.watch(['app/images/**/*'], ['images', run]);
+    gulp.watch(['app/elements/**/*.{scss,css}'], ['styles:elements', run]);
+    gulp.watch(['app/elements/**/*.{js,html}'], ['vulcanize', run]);
+    gulp.watch(['vulcanize.json'], ['vulcanize:common', run]);
+  }
+
+  run();
 });
 
 gulp.task('default', ['clean'], function(cb) {
-  runSequence('build:debug', 'run', cb);
+  runSequence('build', 'run', cb);
 });
