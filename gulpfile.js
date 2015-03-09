59bcf488e85a350c9e288762d2ae9c444436146e
ragingwind <ragingwind@gmail.com>
2015-03-09 22:22:57 +0900

    Remove :dist task

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,44 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var gutil = require('gulp-util');
 var del = require('del');
 var fs = require('fs');
+var path = require('path');
 var runSequence = require('run-sequence');
+var merge = require('merge-stream');
 var next = require('next-promise');
 var ccad = require('cca-delegate');
     ccad.options({verbose: true});
 var polymports = require('gulp-polymports');
 var linkto = require('cordova-linkto');
 
-var config = {
+var env = {
   platform: 'chrome',
   watch: false,
-  optimize: false,
-  target: 'app'
+  target: 'app',
+  configure: function() {
+    var opts = require('minimist')(process.argv.slice(2));
+
+    env.watch = !!opts.watch;
+
+    if (opts.platform && /chrome|android|ios/.test(opts.platform)) {
+      env.platform = (opts.platform || opts.p);
+    }
+
+    if (opts.dist) {
+      env.target = 'dist';
+    }
+  }
 };
 
 var styleTask = function(stylesPath, srcs) {
+  var dest = path.join(env.target, stylesPath);
+  var optimize = env.target === 'dist';
+
   return gulp.src(srcs.map(function(src) {
-      return 'app/' + stylesPath + src;
+      return path.join('app/', stylesPath, src);
     }))
     .pipe($.if('*.scss', $.rename({suffix:'.scss'})))
-    .pipe($.changed('app/' + stylesPath, {extension: '.scss'}))
+    .pipe($.changed(dest, {extension: '.scss'}))
     .pipe($.rubySass({
         style: 'expanded',
         precision: 10
@@ -30,26 +46,20 @@ var styleTask = function(stylesPath, srcs) {
       .on('error', console.error.bind(console))
     )
     .pipe($.if('*.scss', $.rename(function(p) {p.extname += '.css'})))
-    .pipe(gulp.dest('.tmp/' + stylesPath))
-    .pipe($.if('*.css', $.cssmin()))
-    .pipe(gulp.dest('app/' + stylesPath));
+    .pipe($.if(optimize, gulp.dest('.tmp/' + stylesPath)))
+    .pipe($.if('*.css' && optimize, $.cssmin()))
+    .pipe(gulp.dest(dest));
 };
 
-gulp.task('configure', function() {
-  var opts = require('minimist')(process.argv.slice(2));
-
-  config.watch = !!opts.watch;
-  config.optimize = (!!opts.optimize || !!opts.o);
-
-  if (opts.platform || opts.p) {
-    config.platform = (opts.platform || opts.p);
-  }
-
-  if (opts.target) {
-    linkto(opts.target, './platform');
-    config.target = opts.target;
-  }
-});
+var runApp = function() {
+  ccad.run({
+    platform: env.platform,
+    cwd: './platform'
+  }).then(null, function(err) {
+    err & console.log(err.toString());
+    cb(err);
+  });
+};
 
 gulp.task('jshint', function() {
   return gulp.src([
@@ -76,7 +86,7 @@ gulp.task('images', function () {
       progressive: true,
       interlaced: true
     })))
-    .pipe(gulp.dest('build/images'));
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('copy', function() {
@@ -88,29 +98,29 @@ gulp.task('copy', function() {
     dot: true,
     base: './app'
   })
-  .pipe($.changed('build/'))
-  .pipe(gulp.dest('build'));
+  .pipe($.changed('dist/'))
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('copy:bower', function() {
   return gulp.src([
     'app/bower_components/**/*'
   ])
-  .pipe($.changed('build/bower_components'))
-  .pipe(gulp.dest('build/bower_components'));
+  .pipe($.changed('dist/bower_components'))
+  .pipe(gulp.dest('dist/bower_components'));
 });
 
 gulp.task('copy:elements', function() {
   return gulp.src([
     'app/elements/**/*',
-    '!app/elements/**/*.{scss}'
+    '!app/elements/**/*.scss'
     ])
-    .pipe($.changed('build/elements'))
-    .pipe(gulp.dest('build/elements'));
+    .pipe($.changed('dist/elements'))
+    .pipe(gulp.dest('dist/elements'));
 });
 
 gulp.task('vulcanize:common', function() {
-  var dest = './app/bower_components/common-elements';
+  var dest = path.join(env.target, '/bower_components/common-elements');
   var bundles = JSON.parse(fs.readFileSync('./vulcanize.json')).bundles;
 
   polymports.src(bundles)
@@ -124,8 +134,8 @@ gulp.task('vulcanize:common', function() {
 });
 
 gulp.task('vulcanize:elements', function() {
-  var dest = './build/elements/app-main';
-  gulp.src(['build/elements/app-main/app-main.html'])
+  var dest = './dist/elements/app-main';
+  gulp.src('dist/elements/app-main/app-main.html')
     .pipe($.vulcanize({
       dest: dest,
       csp: true,
@@ -151,7 +161,7 @@ gulp.task('html', function () {
       empty: true,
       spare: true
     })))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('manifest', function() {
@@ -163,87 +173,65 @@ gulp.task('manifest', function() {
     .pipe($.if('*.js', $.sourcemaps.init()))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.js', $.sourcemaps.write()))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('vulcanize', function(cb) {
-  var tasks = [];
-
-  tasks.push(['styles:elements', 'copy:elements']);
-  if (config.optimize) {
-    tasks.push('vulcanize:elements');
-  }
-  tasks.push(cb);
-
-  runSequence.apply(runSequence, tasks);
-});
-
-var watchTask = function(postTask) {
-  return function() {
-    var targets = [
-      [['app/**/*.html', '!app/elements/**/*.html'], ['copy']],
-      [['app/scripts/**/*.js'], ['jshint', 'copy']],
-      [['app/styles/**/*.{scss,css}'], ['styles']],
-      [['app/images/**/*'], ['images']],
-      [['app/elements/**/*.{scss,css}'], ['styles:elements']],
-      [['app/elements/**/*.{js,html}'], ['vulcanize']],
-      [['vulcanize.json'], ['vulcanize:common']]
-    ];
-
-    targets.forEach(function(t) {
-      if (postTask) {
-        t[1].push(postTask);
-      }
-      gulp.watch(t[0], t[1]);
-    });
-  }
-};
-
-gulp.task('build', ['configure', 'jshint'], function() {
-  var tasks = [
-    ['styles', 'images', 'vulcanize:common'],
-    ['copy', 'copy:bower'],
-    'vulcanize', 'manifest'
-  ];
-
-  if (config.optimize) {
-    tasks.push('html');
-  }
-
-  if (config.watch) {
-    tasks.push(watchTask());
-  }
-
-  runSequence.apply(runSequence, tasks);
-});
+gulp.task('vulcanize', ['styles:elements', 'copy:elements', 'vulcanize:elements']);
 
 gulp.task('clean', function(cb) {
   del.sync([
-    '.tmp', 'build',
+    '.tmp', '.sass-cache', 'dist',
     'app/bower_components/common-elements',
     'app/elements/**/*.scss.css**',
-    'app/style/*.scss.css**'
+    'app/styles/*.scss.css**'
   ]);
   $.cache.clearAll(cb);
 });
 
-gulp.task('run', ['configure'], function() {
-  var run = function() {
-    ccad.run({
-      platform: config.platform,
-      cwd: './platform'
-    }).then(null, function(err) {
-      err & console.log(err.toString());
-    });
-  }
+gulp.task('build', ['jshint'], function() {
+  if (env.target === 'app') {
+    runSequence(
+      ['styles', 'vulcanize:common'],
+      'styles:elements'
+    );
+  } else {
+    runSequence(
+      ['styles', 'images', 'vulcanize:common'],
+      ['copy', 'copy:bower'],
+      'manifest', 'html', 'vulcanize');
+    }
+});
 
-  run();
+gulp.task('run', function() {
+  linkto(env.target, './platform', function(err) {
+    if (err) {
+      console.log('Failed to change app path to', env.target);
+      return;
+    }
 
-  if (config.watch) {
-    watchTask(run)();
-  }
+    runApp();
+
+    if (env.watch) {
+      if (env.target === 'app') {
+        gulp.watch(['app/scripts/**/*.js'], ['jshint', runApp]),
+        gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', runApp]),
+        gulp.watch(['app/elements/**/*.{scss,css}'], ['styles:elements', runApp]),
+        gulp.watch(['vulcanize.json'], ['vulcanize:common', runApp])
+      } else {
+        gulp.watch(['app/**/*.html', '!app/elements/**/*.html'], ['copy', 'html', runApp]);
+        gulp.watch(['app/scripts/**/*.js'], ['jshint', 'copy', runApp]),
+        gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', runApp]),
+        gulp.watch(['app/images/**/*'], ['images', runApp]),
+        gulp.watch(['app/elements/**/*.{scss,css}'], ['styles:elements', runApp]),
+        gulp.watch(['app/elements/**/*.{js,html}'], ['vulcanize', runApp]),
+        gulp.watch(['vulcanize.json'], ['vulcanize:common', runApp])
+      }
+    }
+  });
 });
 
 gulp.task('default', ['clean'], function(cb) {
   runSequence('build', 'run', cb);
 });
+
+env.configure();
