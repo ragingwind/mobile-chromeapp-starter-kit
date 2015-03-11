e2758776837fa65cee5fd157c3e975b2ab0f6fc5
ragingwind <ragingwind@gmail.com>
2015-03-11 17:55:38 +0900

    Minify procedure

--- configure.js
+++ configure.js
@@ -1,8 +1,8 @@
-var next = require('next-promise');
 var exec = require('exec-then');
-var cp = require('cp-file');
-var args = require('meow')({
-  help: [
+var args = require('minimist')(process.argv.slice(2));
+
+if (args._.length === 0) {
+  console.log([
       'Usage',
       '   node ./configure com.your.company.YourAppName --android --ios',
       '',
@@ -10,59 +10,21 @@ var args = require('meow')({
       '   --ios: Choose iOS platform',
       '   --android: Choose Android platform',
       '   --verbose: Show messages'
-  ].join('\n')
-});
-
-var manifest = [
-  {
-    run: 'npm install',
-    guide: 'Install NPM packages'
-  },
-  {
-    run: 'bower install',
-    guide: 'Install Bower packages'
-  },
-  {
-    run: 'cca create ./platform',
-    guide: 'Create a Chrome Mobile App project',
-  }
-];
-
-if (args.input.length === 0 || Object.keys(args.flags).length === 0) {
-  args.showHelp();
+  ].join('\n'));
   return;
 }
 
-// Update verbose option
-if (args.flags.verbose) {
-  manifest[0].run += ' --verbose';
-  manifest[1].run += ' --verbose';
-  exec.verbose = true;
-}
-
-// Update manifest data
-manifest[2].run = [
-  manifest[2].run,
-  args.input[0],
-  args.flags.android ? '--android' : null,
-  args.flags.ios ? '--ios' : null,
+var bin = [
+  'cca create ./platform',
+  args._[0],
+  args.android ? '--android' : '',
+  args.ios ? '--ios' : '',
   '--link-to=app/manifest.json'
 ].join(' ');
 
-// Run commands in the manifest
-next(manifest, function(run) {
-  console.info(run.guide);
-
-  return exec(run.run, {verbose: args.flags.verbose}, function(res, deferred) {
-    if (res.err) {
-      deferred.reject(res.err);
-    }
-  });
-}).then(function() {
-  console.log('done');
-}, function (err) {
-  console.log('You\'ve got problems. Check out errors');
-  console.log(err ? err.toString() : 'Failed to create in Unknown reason');
+exec(bin, {verbose: args.verbose}, function(res) {
+  if (res.err) {
+    console.log('You\'ve got problems. Check out errors');
+    console.log(res.err ? res.err.toString() : 'Failed to create in Unknown reason');
+  }
 });
-
-
