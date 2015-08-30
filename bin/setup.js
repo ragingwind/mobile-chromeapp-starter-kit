45f84510ba1011d6a5aea0c22dba5b694306db2b
ragingwind <ragingwind@gmail.com>
2015-08-30 21:15:13 +0900

    Renewer chrome-app path

--- bin/setup.js
+++ bin/setup.js
@@ -21,16 +21,16 @@ console.log('Setup will be started with %s, %s', name, platforms);
 var cmd = [
   'git submodule update --init --recursive',
   'cp -r bin/chrome src/chrome',
-  // ['cca create ./platform', name, platforms, '--link-to=./src/manifest.json'].join(' '),
-  // 'cd src/polymer-starter-kit',
-  // 'npm install && bower install',
-  // 'gf-import ./bower_components/font-roboto/roboto.html --html=./bower_components/font-roboto/'
+  ['cca create ./platform', name, platforms, '--link-to=./src/chrome/manifest.json'].join(' '),
+  'cd src',
+  'npm install && bower install',
+  'gf-import ./bower_components/font-roboto/roboto.html --html=./bower_components/font-roboto/'
 ].join(' && ');
 
 mkdirp.sync('./platform');
 
 var exec = shell.exec(cmd, {async: true}, function() {
-  // replacePath();
+  replacePath();
 
   console.log('Setup has been done');
 });
