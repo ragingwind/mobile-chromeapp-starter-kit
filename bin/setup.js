7643f646b049c55aa3597da8f9792d2a32996f5d
ragingwind <ragingwind@gmail.com>
2015-08-30 20:21:40 +0900

    Revision structure of path

--- bin/setup.js
+++ bin/setup.js
@@ -1,23 +1,36 @@
 #!/usr/bin/env node
 'use strict';
 
+var fs = require('fs');
 var mkdirp = require('mkdirp');
 var shell = require('shelljs');
 var name = process.argv[2] || 'com.my.app';
 var platforms = process.argv[3] || '--android';
 
+function replacePath() {
+  var gulpfile = './src/gulpfile.js';
+  var content = fs.readFileSync(gulpfile);
+
+  content = content.toString().replace(/\'dist/gi, '\'chrome/dist');
+  fs.writeFileSync(gulpfile, content);
+}
+
 console.log(process.argv);
-console.log('Setup for CCA project will be started with %s, %s', name, platforms);
+console.log('Setup will be started with %s, %s', name, platforms);
 
 var cmd = [
   'git submodule update --init --recursive',
-  'cd src/polymer-starter-kit; npm install && bower install',
-  'gf-import ./bower_components/font-roboto/roboto.html --html=./bower_components/font-roboto/',
-  ['cca create ./platform', name, platforms, '--link-to=./src/manifest.json'].join(' ')
+  'cp -r bin/chrome src/chrome',
+  // ['cca create ./platform', name, platforms, '--link-to=./src/manifest.json'].join(' '),
+  // 'cd src/polymer-starter-kit',
+  // 'npm install && bower install',
+  // 'gf-import ./bower_components/font-roboto/roboto.html --html=./bower_components/font-roboto/'
 ].join(' && ');
 
 mkdirp.sync('./platform');
 
 var exec = shell.exec(cmd, {async: true}, function() {
-  console.log('obj');
+  replacePath();
+
+  console.log('Setup has been done');
 });
